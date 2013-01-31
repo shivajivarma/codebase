7dc7f699b38ccf45f83ac4c8f7953303618de07e
shivajivarma <shivaji.mailbox@gmail.com>
2013-01-31 15:03:26 +0530

    Sorting

--- Sorting/quick-sort.c
+++ Sorting/quick-sort.c
@@ -1,16 +1,3 @@
-/*
-Problem statement: To sort set of elements in an array.
-
-Input:
-N // array size.
-arr[ ] 	// array
-Output: Sorted array.
-
-Eg:
-input: 		5	7	6	3	10	2	4	1
-output:		1	2	3	4	5	6	7	10
-*/
-
 #include<stdio.h>
 
 void swap(int *m,int *n)
