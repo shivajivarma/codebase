3d4f7797944a7c959fcd0b54c7b64c7bb682fc06
shivajivarma <postman@shivajivarma.com>
2013-01-31 12:55:45 +0530

    Documented

--- towers-of-hanoi.c
+++ towers-of-hanoi.c
@@ -1,10 +1,15 @@
 /*
-The Tower of Hanoi (also called the Tower of Brahma or Lucas' Tower) is a mathematical game or puzzle. It consists of three rods, and a number of disks of different sizes which can slide onto any rod. The puzzle starts with the disks in a neat stack in ascending order of size on one rod, the smallest at the top, thus making a conical shape.
+The Tower of Hanoi (also called the Tower of Brahma or Lucas' Tower) is a mathematical game or puzzle. 
+It consists of three rods, and a number of disks of different sizes which can slide onto any rod. 
+The puzzle starts with the disks in a neat stack in ascending order of size on one rod, 
+the smallest at the top, thus making a conical shape.
 
 The objective of the puzzle is to move the entire stack to another rod, obeying the following rules:
-> Only one disk may be moved at a time.
-> Each move consists of taking the upper disk from one of the rods and sliding it onto another rod, on top of the other disks that may already be present on that rod.
-> No disk may be placed on top of a smaller disk.
+# Only one disk may be moved at a time.
+# Each move consists of taking the upper disk from one of the rods and sliding it onto another rod, 
+	on top of the other disks that may already be present on that rod.
+# No disk may be placed on top of a smaller disk.
+
 */
 
 #include<stdio.h>
