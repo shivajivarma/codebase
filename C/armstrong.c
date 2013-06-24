bc5a3874824f6e7800e73567ff7d5207e80accaf
Shivaji Varma <shivaji.mailbox@gmail.com>
2013-06-24 14:58:06 +0530

    :P

--- C/armstrong.c
+++ C/armstrong.c
@@ -2,7 +2,7 @@
 
 An Armstrong number is a number such that the sum of its digits raised to the third power is equal to the number itself.  
 For example, 371 is an Armstrong number, since 3^3 + 7^3 + 1^3 = 371.
-Another example, 153. 1^3 + 5^3 + 3^3 = 3 + 15 + 9 = 153.
+Another example, 153. 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153.
 
 */
 
