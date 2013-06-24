51241bb9dc90c7586855b31d1429208b9fd1ae46
Shivaji Varma <shivaji.mailbox@gmail.com>
2013-06-24 15:33:15 +0530

    :P

--- C/gcd.c
+++ C/gcd.c
@@ -26,10 +26,12 @@ int main(){
     printf("GCD of two numbers\nEnter x and y:: ");
     scanf("%d%d",&x,&y);
     
-    if(x<y) printf("\n GCD:: %d",gcd(x,y));
-    else
-      printf("\n GCD:: %d",gcd(y,x));
-  
+    if(x==0&&y==0) printf("Error: Atleast one of the interger must not be zero.")
+    else{
+      if(x<y) printf("\n GCD:: %d",gcd(x,y));
+      else
+        printf("\n GCD:: %d",gcd(y,x));
+    }
     return 0;
 }
 
