548b3917db2d3f799ff27504476a45dab6f0dea7
Shivaji Varma <shivaji.mailbox@gmail.com>
2013-06-26 11:59:13 +0530

    :P

---
+++ JAVA/palidrome.java
@@ -0,0 +1,44 @@
+/*
+
+ A string is called palindrome if it symmetric. In other word a string is called palindrome if string remains same if its characters are reversed. 
+ For example: asdsa
+ If we will reverse it will remain same i.e. malayalam
+
+Example of string palindrome:  a,b, aa,aba,madam etc.
+
+*/
+
+import java.io.*;
+ 
+class palindrome{
+ 
+ 
+ 
+ void palindrome(String str){
+  /* Alogorithm for checking whether the given string is palindrome or not*/
+  String reverse=new String();
+  
+    for(int i=str.length()-1;i>=0;i--){
+     System.out.println(reverse+"\n");
+     reverse=reverse+str.charAt(i);
+    }
+  
+    if(str.equals(reverse)) System.out.println("Given string is a Palindrome");
+    else System.out.println("Not a Palindrome");
+  /* End of the algorithm */
+ }
+ 
+ 
+ public static void main(String args[]){
+
+  String str=new String(args[0]);
+    palindrome(str);
+    
+ }
+ 
+}
+
+/*
+With regards,
+Shivaji Varma
+*/
