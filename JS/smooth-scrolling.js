1645977be08ce747a6b3794ee8d06137615ab0fc
Shivaji Varma <contact@shivajivarma.com>
2014-01-21 11:14:04 +0530

    Create smooth-scrolling.js

---
+++ JS/smooth-scrolling.js
@@ -0,0 +1,21 @@
+var $root = $('html, body');
+
+$('a').click(function() {
+  var href = $.attr(this, 'href');
+  var hash;
+    
+  if(href == '#'){
+    hash = $('body');
+  }else if(href.match(/^#[a-zA-Z0-9]+$/g)){
+    hash = $('[name='+href.substring(1)+']');
+  }else 
+	return true;
+	
+  $root.animate({
+      scrollTop: $(hash).offset().top
+    }, 500, function () {
+      window.location.hash = href;
+  });
+ 
+  return false;
+});
