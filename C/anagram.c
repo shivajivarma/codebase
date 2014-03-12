af0a2b5b6297a7663847152e7eb1ef9d41f33682
Shivaji Varma <contact@shivajivarma.com>
2014-03-12 16:38:33 +0530

    Create anagram.c

---
+++ C/anagram.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main() {
+
+    
+  char a[6]="aklbc",b[6]="bcakk";
+  int i,j,anagram=1;
+  
+  if(strlen(a)==strlen(b))
+  for(i=0;i<strlen(a);i++){
+      for(j=0;j<strlen(b);j++){
+           if(a[i]==b[j]){
+            memmove( &a[ 0 ] , &a[ 1 ], strlen( a ));
+            i--;
+            memmove( &b[ j ] , &b[ j + 1 ], strlen( b ) - j);
+            break;
+           }
+        }
+        if(j == strlen(b) + 1){
+            anagram=0;
+            break;
+        }
+  }
+  else anagram=0;
+  
+  if(anagram == 1)
+   printf("anagram");
+   else printf("not an anagram");
+
+  return 0;
+}
