ba0591a90b0bd24ee358bdb217eb80d3fb44514e
Shivaji Varma <contact@shivajivarma.com>
2014-06-11 16:57:36 +0530

    Create canItFormPalidrome.py

---
+++ PYTHON/canItFormPalidrome.py
@@ -0,0 +1,19 @@
+'''
+Can one of the permutation of given string, forms a palindrome or not?
+Eg:
+ssm
+
+It can be resolved to sms, which is a palindrome.
+'''
+
+def canItFormPalindrome(str):
+	odd = 0
+	while(len(str)>0):
+		if(str.count(str[0])%2==1):
+			odd = odd + 1
+		if(odd > 1):
+			return False
+		str = str.replace(str[0],"")
+	return True
+
+print canItFormPalindrome(raw_input())
