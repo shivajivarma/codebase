56f5931571813d64742eff68852b4163bfe7ad48
Shivaji Varma <contact@shivajivarma.com>
2014-06-13 14:29:25 +0530

    Update armstrong.java

--- JAVA/armstrong.java
+++ JAVA/armstrong.java
@@ -8,27 +8,40 @@ Another example, 153. 1^3 + 5^3 + 3^3 = 3 + 15 + 9 = 153.
 
 import java.util.*;
 
-class armstrong {
-	public static void main(String[] args) {
-		
-		int n = 0,sum=0,temp,r;
-		Scanner in= new Scanner(System.in);
-		System.out.println("Enter number");	
-		n=in.nextInt();
+class Common {
 	
-		temp=n;
-	
+	public static boolean Armstrong(int number){
+		int  sum=0,temp,r;
+		temp=number;
 		while (temp!=0) {
 			r=temp%10;
 			sum=sum+(r*r*r);
 			temp=temp/10;
 		}
-
-
-		if (n==sum)
-			System.out.println("Entered number is amstrong"); 
+		if(number == sum)
+			return true;
 		else
-			System.out.println("Not amstrong");
+			return false;
+
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner in= new Scanner(System.in);
+		System.out.print("Enter number: ");	
+		try{
+			int n=in.nextInt();
+			if (Armstrong(n))
+				System.out.println(n + " is an amstrong number"); 
+			else
+				System.out.println(n + " is not an amstrong number");
+		}
+		catch(InputMismatchException e){
+			System.out.println("Error: Only interger numbers are accepted");
+		}
+		finally{
+			in.close();
+		}
 	}
 }
 
