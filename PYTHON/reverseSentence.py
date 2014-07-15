8096eba2233b4d77d2082a91ba44dadbf09bf7ad
Shivaji Varma <contact@shivajivarma.com>
2014-07-15 16:17:19 +0530

    Update reverseSentence.py

--- PYTHON/reverseSentence.py
+++ PYTHON/reverseSentence.py
@@ -1,3 +1,11 @@
+'''
+Program to print the sentence in reverse.
+Eg: 
+  input: There is a cat on the wall
+  output: wall the on cat a is There
+
+The works in Python 3 or above.
+'''
 sentence = raw_input()
 word = sentence.split()
 for i in reversed(word): print(i,end=" ")
