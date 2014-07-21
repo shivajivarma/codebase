163f8ce60fc97dac062e1e0afc837cf5cdf160be
Shivaji Varma <contact@shivajivarma.com>
2014-07-21 11:42:41 +0530

    updated

--- JAVA/OneBy2PowerN.java
+++ JAVA/OneBy2PowerN.java
@@ -5,20 +5,31 @@ Compute exact value of 1/(2^n) for T cases
 import java.util.Scanner;
 import java.math.BigInteger;
 
-class OneBy2PowerN{
-    public static void main(String args[]) {
-		 Scanner in = new Scanner(System.in);
-		 int T = in.nextInt(),len,num,i,j;
-		 BigInteger b,five = BigInteger.valueOf(5);
-		 String output;
-		 for(i=0;i<T;i++){
-			 num = in.nextInt();
-			 b = five.pow(num);
-			 len = num - b.toString().length();
-			 output="0.";
-			 for(j=0;j<len;j++)
-			 output = output + "0";
-			 System.out.println(output+b);
-		 }
-	  }
+class OneBy2PowerN {
+	
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Enter number of test cases: ");
+		int T = in.nextInt(), i,n;
+		
+		for (i = 0; i < T; i++) {
+			System.out.print("\nEnter value of N: ");
+			n = in.nextInt();
+			System.out.println("1/(2^"+n+") = "+oneBy2PowerN(n));	
+		}
+	}
+	
+	static String oneBy2PowerN(int input){
+		BigInteger b, five = BigInteger.valueOf(5);
+		String output= "0.";
+		b = five.pow(input);
+		int j, len;
+		len = input - b.toString().length();
+		
+		for (j = 0; j < len; j++)
+			output = output + "0";
+		
+		return output+b;
+	}
 }
