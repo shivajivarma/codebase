7077419622e5a4ddaf5bc9a8fbc6edf34ee84adf
Shivaji Varma <contact@shivajivarma.com>
2014-07-21 14:02:24 +0530

    Update one-by-2-power-n.py

--- PYTHON/one-by-2-power-n.py
+++ PYTHON/one-by-2-power-n.py
@@ -1,9 +1,23 @@
-T = int(raw_input())
-for i in xrange(0, T):
-	num = int(raw_input())
-	b = pow(5,num)
-	l = num - len(str(b))
-	output = "0."
-	for j in xrange(0, l):
-		output = output + "0"
-	print output + str(b)
+'''
+    Compute exact value of 1/(2^n) for T cases
+'''
+def oneBy2PowerN(N):
+    b = pow(5,N)
+    l = N - len(str(b))
+    output = "0."
+    for j in range(0, l):
+        output = output + "0"
+    return output + str(b)
+
+print("Enter number of test cases: ",end="")
+T = int(input())
+
+for i in range(0, T):
+    print("\nEnter the value of N: ",end="")
+    num = int(input())
+    print(oneBy2PowerN(num))
+	
+'''
+With Regards,
+Shivaji Varma
+'''
