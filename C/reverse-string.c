8aca3dc6a58abceb95afefe45423f9ca00dec8b8
Shivaji Varma <contact@shivajivarma.com>
2014-07-30 17:59:24 +0530

    Update reverse-string.c

--- C/reverse-string.c
+++ C/reverse-string.c
@@ -1,20 +1,23 @@
 /*
-
 Program to reverse a portion of a stentence from postion start to end.
 
 Eg: 
-Input: Hello world
-Start postion: 0
-End postion: .
+Enter a string: hello world
+Enter start and end positions: 0 4
 
+Reverse of portion of the sentence:
+olleh world
 
+Reverse of entire sentence:
+dlrow olleh
 */
 
 #include <stdio.h>
+#include <string.h>
 
-//	Function to reverse portion of string from position 'start' to 'end'. 
+//	Function to reverse portion of string from position 'start' to 'end'.
 void reverseString(char * str,int start,int end){
-	int i, len=end-start; 
+	int i, len=(end-start)+1; 
 	char temp;
 	
 	for(i=0,str=str+start;i<len/2;i++){
@@ -26,12 +29,20 @@ void reverseString(char * str,int start,int end){
 
 
 void main(){
-	char str[20];
+	char str[20],revStr[20];
+	int start, end;
 	
 	printf("Enter a string: ");
 	gets(str);
+	strcpy(revStr,str);
 	
-	reverseString(str,0,strlen(str));
+	printf("Enter start and end positions: ");
+	scanf("%d %d",&start,&end);
+	reverseString(str,start,end);
+	printf("\nReverse of portion of the sentence:");
+	printf("\n%s\n",str);
 	
-	printf("%s",str);
+	printf("\nReverse of entire sentence:");
+	reverseString(revStr,0,strlen(revStr)-1);
+	printf("\n%s",revStr);
 }
