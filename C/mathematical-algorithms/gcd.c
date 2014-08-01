b4737d99b7947b5083862ded4559d9a075162f83
Shivaji Varma <contact@shivajivarma.com>
2014-08-01 16:21:44 +0530

    Update gcd.c

--- C/mathematical-algorithms/gcd.c
+++ C/mathematical-algorithms/gcd.c
@@ -1,23 +1,20 @@
 /*
-
 The greatest common divisor (GCD) of two or more integers (at least one of which is not zero), 
 is the largest positive integer that divides the numbers without a remainder.
 For example, the GCD of 8 and 12 is 4.
 
 NOTE: The following program computes gcd of two numbers.
-
 */
 
 #include<stdio.h>
 
-  int gcd(int a,int b)
-  {  
-    int c=b%a;
-    
-    if(c==0) return a;
-    return gcd(c,a);
-  }
-
+int gcd(int a,int b)
+{  
+  int c=b%a;
+  
+  if(c==0) return a;
+  return gcd(c,a);
+}
 
 int main()
 {
@@ -26,11 +23,14 @@ int main()
     printf("GCD of two numbers\nEnter x and y:: ");
     scanf("%d%d",&x,&y);
     
-    if(x==0&&y==0) printf("Error: Atleast one of the interger must not be zero.");
-    else{
-      if(x<y) printf("\n GCD:: %d",gcd(x,y));
-      else
+    if(x==0&&y==0){
+      printf("Error: Atleast one of the interger must not be zero.");
+    } else{
+      if(x<y){
+        printf("\n GCD:: %d",gcd(x,y));
+      }else{
         printf("\n GCD:: %d",gcd(y,x));
+      }
     }
     return 0;
 }
