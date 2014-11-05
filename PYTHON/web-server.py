00e7e85059055dbeeb7772061287054a5a253649
Shivaji Varma <contact@shivajivarma.com>
2014-11-05 11:20:12 +0530

    Update web-server.py

--- PYTHON/web-server.py
+++ PYTHON/web-server.py
@@ -17,3 +17,28 @@ httpd = ServerClass(server_address, HandlerClass)
 sa = httpd.socket.getsockname()
 print("Serving HTTP on", sa[0], "port", sa[1], "...")
 httpd.serve_forever()
+
+'''
+Python older Versions
+
+import sys
+import BaseHTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+
+
+HandlerClass = SimpleHTTPRequestHandler
+ServerClass  = BaseHTTPServer.HTTPServer
+Protocol     = "HTTP/1.0"
+
+if sys.argv[1:]:
+    port = int(sys.argv[1])
+else:
+    port = 8000
+
+HandlerClass.protocol_version = Protocol
+httpd = ServerClass(('127.0.0.1', port), HandlerClass)
+
+sa = httpd.socket.getsockname()
+print "Serving files on", sa[0], "port", sa[1], "..."
+httpd.serve_forever()
+'''
