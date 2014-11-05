f6770c1a518fcefd7f5000f67cc0d5f9cae7e990
Shivaji Varma <contact@shivajivarma.com>
2014-11-05 23:39:30 +0530

    Update alexa-rank.py

--- PYTHON/alexa-rank.py
+++ PYTHON/alexa-rank.py
@@ -1,11 +1,11 @@
-import urllib
-from xml.dom import minidom
+import urllib.request
+import xml.dom.minidom
 
 ALEXA_URL = 'http://data.alexa.com/data?cli=10&dat=snbamz&url='
 
 def rank(site):
 	url = ALEXA_URL + site
-	dom = minidom.parse(urllib.urlopen(url))
-	return dom.getElementsByTagName("REACH")[0].getAttribute("RANK")
+	dom = xml.dom.minidom.parse(urllib.request.urlopen(url))
+	return dom.getElementsByTagName("REACH")[0].getAttribute("RANK"),dom.getElementsByTagName("CLAIMED")[0].getAttribute("DATE")
 
-print rank('shivajivarma.com')
+print(rank('shivajivarma.com'))
