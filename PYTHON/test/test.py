ddfbfe048872dd23b4f8ae901e303f9f503f16a1
Shivaji Varma <contact@shivajivarma.com>
2014-11-17 12:36:31 +0530

    Update test.py

--- PYTHON/test/test.py
+++ PYTHON/test/test.py
@@ -1,11 +1,1 @@
-language: python
-python:
-  - "2.6"
-  - "2.7"
-  - "3.2"
-  - "3.3"
-# command to install dependencies
-script: python PYTHON\test\test.py
-branches:
-  only:
-    - master
+return true
