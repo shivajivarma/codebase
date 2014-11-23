32d74a1239a24e43ab808ad035bcb6135153681b
Shivaji Varma <contact@shivajivarma.com>
2014-11-23 07:57:21 +0530

    Update randomString.js

--- JS/randomString.js
+++ JS/randomString.js
@@ -1,9 +1,11 @@
 function randomString(len) {
 	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
 	var randomstring = '';
+	
 	for (var i=0; i<len; i++) {
 		var rnum = Math.floor(Math.random() * chars.length);
 		randomstring += chars.substring(rnum,rnum+1);
 	}
+	
 	return randomstring;
 }
