c99e4462c6290471a54b4d55a3522a040e635118
Shivaji Varma <contact@shivajivarma.com>
2014-11-30 21:47:32 +0530

    Update bit-count.cpp

--- CPP/bit-count.cpp
+++ CPP/bit-count.cpp
@@ -3,13 +3,12 @@
 #include <string>         // std::string
 #include <bitset>         // std::bitset
 
-int main ()
-{
-	unsigned long long l=18446744073709551614;
+int main (){
+  unsigned long long l=18446744073709551614;
   std::bitset<64> foo(l);
   
   
- // std::cout << (foo^=bar).count();
+  // std::cout << (foo^=bar).count();
   std::cout << l << " has ";
   std::cout << foo.count() << " ones and ";
   std::cout << (foo.size()-foo.count()) << " zeros.\n";
