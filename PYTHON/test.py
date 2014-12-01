d44153000183ea6487a12538c4702acf91076c1f
Shivaji Varma <contact@shivajivarma.com>
2014-12-01 15:55:37 +0530

    Update test.py

--- PYTHON/test.py
+++ PYTHON/test.py
@@ -1,2 +1,11 @@
 from canItFormPalindrome import canItFormPalindrome
+
+/* Valid outputs for canItFormPalindrome */
 canItFormPalindrome("teest")
+canItFormPalindrome("malayalam")
+canItFormPalindrome("kkeesii")
+
+/* In-valid outputs canItFormPalindrome */
+!canItFormPalindrome("tttie")
+!canItFormPalindrome("ioplo")
+!canItFormPalindrome("kokokokolllj")
