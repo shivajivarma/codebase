8492f3447c61a5d9bb4bb9077b68d1cacbbe291e
Shivaji Varma <contact@shivajivarma.com>
2014-12-02 07:13:42 +0530

    Update gcd.c

--- C/mathematical-algorithms/gcd.c
+++ C/mathematical-algorithms/gcd.c
@@ -9,27 +9,26 @@ NOTE: The following program computes gcd of two numbers.
 #include<stdio.h>
 
 // returns GCD of a,b where a<b
-int gcd(int a,int b)
-{  
-  int c=b%a;
-  
-  if(c==0) return a;
-  return gcd(c,a);
+int gcd(int a,int b){  
+  if(b%a == 0) 
+    return a;
+  else
+    return gcd(c,a);
 }
 
-int main()
-{
+int main(){
+  
     int x,y;
     
     printf("GCD of two numbers\nEnter x and y:: ");
     scanf("%d%d",&x,&y);
     
-    if(x==0&&y==0){
+    if(x == 0 && y == 0){
       printf("Error: Atleast one of the interger must not be zero.");
     } else{
-      if(x<y){
+      if(x < y){
         printf("\n GCD:: %d",gcd(x,y));
-      }else{
+      } else{
         printf("\n GCD:: %d",gcd(y,x));
       }
     }
