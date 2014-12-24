426b834d2ecb8d87be5b3c3c971ceb4c832e5a58
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2014-12-24 12:57:28 +0530

    Update leap-year.c

--- C/leap-year.c
+++ C/leap-year.c
@@ -1,23 +1,28 @@
 /* C program to check whether a year is leap year or not using if else statement.*/
 
 #include <stdio.h>
+
+int isLeapYear(int year){
+    if(year%4 == 0){
+        if( year%100 == 0){ /* Checking for a century year */
+            if ( year%400 == 0)
+                return 1;
+            else
+                return 0;
+        }else
+            return 1;
+    }else
+        return 0;
+}
+
 int main(){
       int year;
       printf("Enter a year: ");
       scanf("%d",&year);
-      if(year%4 == 0)
-      {
-          if( year%100 == 0) /* Checking for a century year */
-          {
-              if ( year%400 == 0)
-                 printf("%d is a leap year.", year);
-              else
-                 printf("%d is not a leap year.", year);
-          }
-          else
-             printf("%d is a leap year.", year );
+      if(isLeapYear(year) == 1){
+          printf("%d is a leap year.", year);
+      }else{
+           printf("%d is not a leap year.", year);
       }
-      else
-         printf("%d is not a leap year.", year);
       return 0;
 }
