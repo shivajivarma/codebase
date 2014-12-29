b7cde11e9cf9be15ec8e250f609e80b41f24b1bf
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2014-12-29 15:07:04 +0530

    Update Armstrong.java

--- JAVA/Armstrong.java
+++ JAVA/Armstrong.java
@@ -1,11 +1,3 @@
-/*
-
-An Armstrong number is a number such that the sum of its digits raised to the third power is equal to the number itself.  
-For example, 371 is an Armstrong number, since 3^3 + 7^3 + 1^3 = 371.
-Another example, 153. 1^3 + 5^3 + 3^3 = 3 + 15 + 9 = 153.
-
-*/
-
 import java.util.*;
 
 class Armstrong {
@@ -45,8 +37,3 @@ class Armstrong {
 		}
 	}
 }
-
-/*
-With regards,
-Shivaji Varma
-*/
