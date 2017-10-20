deec8dffa0a80548e103c1b2301b7e818f3ba617
Shivaji <shivaji.varma@kagamierp.com>
2017-10-20 11:03:27 +0530

    moved c programs to codebase-c

--- C/system-call/system-call.c
+++
@@ -1,8 +0,0 @@
-#include<stdlib.h>
- 
-int main()
-{
-   system("C:\\Windows\\System32\\ipconfig");
- 
-   return 0;
-}
--- C/temperature-converter/temperature-converter.c
+++
@@ -1,7 +0,0 @@
-float farenheitToCelsius(long f) {
-	return (f - 32)/1.8;	
-}
-
-float celsiusToFarenheit(long c) {
-	return (c * 1.8) + 32;	
-}
